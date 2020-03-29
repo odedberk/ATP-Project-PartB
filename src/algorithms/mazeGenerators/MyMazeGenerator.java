2afc80a6aaac29becec511d0d204922bd5a087ff
eilam gal <eilamg@post.bgu.ac.il>
2020-03-29 20:31:48 +0300

    3 commit

--- src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -3,6 +3,23 @@ package algorithms.mazeGenerators;
 public class MyMazeGenerator extends AMazeGenerator {
     @Override
     public Maze generate(int row, int col) {
+        int[][] newMaze =new int[row][col];
+        for (int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(i%2==0)
+                    newMaze[i][j]=1;
+                else
+                    newMaze[i][j]=0;
+            }
+        }
+        for (int j=0; j<col; j++){
+            for(int i=0; i<row; i++){
+                if(j%2==0)
+                    newMaze[i][j]=1;
+                else
+                    newMaze[i][j]=0;
+            }
+        }
         return null;
     }
 }
