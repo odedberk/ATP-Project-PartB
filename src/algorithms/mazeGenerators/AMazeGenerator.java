aa1391add570f240e05b7eadbbe8fcec20270238
eilam gal <eilamg@post.bgu.ac.il>
2020-03-29 17:45:11 +0300

    first commit

---
+++ src/algorithms/mazeGenerators/AMazeGenerator.java
@@ -0,0 +1,10 @@
+package algorithms.mazeGenerators;
+
+public abstract class AMazeGenerator implements IMazeGenerator {
+
+
+    @Override
+    public long measureAlgorithmTimeMillis(int row, int col) {
+        return 0;
+    }
+}
--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -1,7 +1,6 @@
 package algorithms.mazeGenerators;
 
 public class Maze {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
+
+
 }
