db8b5f4e5cf08c7146dd738d913ff5e1e525cbc7
oded <odedodge@gmail.com>
2020-04-03 19:03:31 +0300

    MazeState Position

--- src/algorithms/search/MazeState.java
+++ src/algorithms/search/MazeState.java
@@ -3,8 +3,17 @@ package algorithms.search;
 import algorithms.mazeGenerators.Position;
 
 public class MazeState extends AState{
+    Position pos;
     public MazeState(double cost, Position p, AState prev) {
         super(p.toString(),cost,prev);
+        pos=p;
     }
 
+    public Position getPos() {
+        return pos;
+    }
+
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
 }
