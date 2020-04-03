c32a1f7d408ab9c8abaddf7372ececb5ae05914c
eilam gal <eilamg@post.bgu.ac.il>
2020-04-03 17:47:23 +0300

    3 commit

--- src/algorithms/search/AState.java
+++ src/algorithms/search/AState.java
@@ -1,5 +1,54 @@
 package algorithms.search;
 
-public abstract class AState {
+import java.util.Objects;
 
+public abstract class AState {
+    private String state;
+    private double cost;
+    private AState ameFrom;
+
+    public AState(String state, double cost, AState ameFrom) {
+        this.state = state;
+        this.cost = cost;
+        this.ameFrom = ameFrom;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public AState getAmeFrom() {
+        return ameFrom;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setAmeFrom(AState ameFrom) {
+        this.ameFrom = ameFrom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AState aState = (AState) o;
+        return Double.compare(aState.cost, cost) == 0 &&
+                Objects.equals(state, aState.state) &&
+                Objects.equals(ameFrom, aState.ameFrom);
+    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(state, cost, ameFrom);
+//    }
 }
