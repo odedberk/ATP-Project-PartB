fa5d147ea46ae8ccbc01291d7fc82ab704f63743
eilam gal <eilamg@post.bgu.ac.il>
2020-04-04 18:12:38 +0300

    3 commit

--- src/algorithms/search/DepthFirstSearch.java
+++ src/algorithms/search/DepthFirstSearch.java
@@ -5,19 +5,18 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class DepthFirstSearch extends ASearchingAlgorithm {
-    //private HashSet<AState> Blacks;
-    //private HashSet<AState>Grays;
     private HashSet<AState> visited;
     private int nodesVisit;
 
     @Override
     public Solution solve(ISearchable s) {
-        //Blacks = new HashSet<AState>();
-       // Grays = new HashSet<AState>() ;
+        if(s.getStartState().equals(s.getGoalState()))
+            return new Solution(s.getStartState());
         visited= new HashSet<AState>();
         nodesVisit=0;
         Stack<AState> rStack = new Stack<AState>();
         rStack.push(s.getStartState());
+        visited.add(s.getStartState());
         while (!rStack.empty()){
             AState state = rStack.pop();
             nodesVisit++;
@@ -25,43 +24,24 @@ public class DepthFirstSearch extends ASearchingAlgorithm {
             for(AState son : s.getAllSuccessors(state)) {
                 if(son.equals(s.getGoalState()))
                     return new Solution(son);
-                if (!visited.contains(son))
+                if (!visited.contains(son)) {
                     rStack.push(son);
+                    visited.add(son);
+                }
             }
-            visited.add(state);
         }
         return new Solution(null);
       }
 
-//    private Solution visit(ISearchable s,AState state){
-//        nodesVisit++;
-//        System.out.println(nodesVisit);
-//        if(state.equals(s.getGoalState()))
-//            return new Solution(state);
-//        visited.add(state);
-//        Solution sol=null;
-//        for(AState son : s.getAllSuccessors(state)) {
-//            if (!visited.contains(son)) {
-//                sol = visit(s, son);
-//            }
-//            if(sol!=null)
-//                break;
-//        }
-//        return sol;
-//    }
 
     @Override
     public int getNumberOfNodesEvaluated() {
         return nodesVisit;
     }
 
-    @Override
-    public int getNumberOfNodesEvaluated() {
-        return 0;
-    }
 
     @Override
     public String getName() {
-        return null;
+        return "DFS";
     }
 }
