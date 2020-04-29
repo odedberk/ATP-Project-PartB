4e99ffee6ef457479b0b9d93c8b5fb23b97b681f
eilam gal <eilamg@post.bgu.ac.il>
2020-04-29 21:27:20 +0300

    Merge branch 'master' of https://github.com/odedberk/ATP-Project-PartA
     Conflicts:
    	out/production/ATP-Project-PartA/algorithms/mazeGenerators/SimpleMazeGenerator.class
    	out/production/ATP-Project-PartA/algorithms/search/ASearchingAlgorithm.class
    	out/production/ATP-Project-PartA/algorithms/search/AState.class
    	out/production/ATP-Project-PartA/algorithms/search/BestFirstSearch.class
    	out/production/ATP-Project-PartA/algorithms/search/BreadthFirstSearch.class
    	out/production/ATP-Project-PartA/algorithms/search/DepthFirstSearch.class
    	out/production/ATP-Project-PartA/test/RunSearchOnMaze.class

--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -22,21 +22,24 @@ public class MyDecompressorInputStream extends InputStream {
     }
     public int read(byte[] byteArray)throws IOException{
         //byte []newByteArray = new byte[12];
-        //byte []compressedMaze = new byte[1000000000];
+       // byte []compressedMaze = new byte[1000000000];
+
         //in.read(compressedMaze);
+        //ArrayList<Integer> temp =
         LinkedList<Integer> unCompressMaze= new LinkedList<>();
         for(int i=0; i<12 ; i++)
+            //byteArray[i]=(byte)compressedMaze[i];
             unCompressMaze.add(in.read());
         int sizeOfPair =in.read();
         LinkedList<Pair<Integer,Integer>> dictionary = getDictionary(sizeOfPair);
-       // unCompressMaze.add(dictionary.get(0).getKey());
+        unCompressMaze.add(dictionary.get(0).getKey());
         for(int i=1; i<dictionary.size(); i++){
             Stack<Integer> temp = new Stack<>();
             if(i != dictionary.size()-1)
                temp.add(dictionary.get(i).getKey());
             int p = dictionary.get(i).getValue();
             while (p>=1){
-                temp.add(dictionary.get(p).getKey());
+                temp.add(dictionary.get(p-1).getKey());
                 p=dictionary.get(p).getValue();
             }
             while (!temp.isEmpty())
