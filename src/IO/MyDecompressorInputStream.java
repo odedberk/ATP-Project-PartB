1ccfd1ab0a83f6faa70709c67f02fbbbed809101
eilam gal <eilamg@post.bgu.ac.il>
2020-04-29 22:23:32 +0300

    Merge branch 'master' of https://github.com/odedberk/ATP-Project-PartA
     Conflicts:
    	out/production/ATP-Project-PartA/algorithms/mazeGenerators/SimpleMazeGenerator.class
    	out/production/ATP-Project-PartA/algorithms/search/ASearchingAlgorithm.class
    	out/production/ATP-Project-PartA/algorithms/search/AState.class
    	out/production/ATP-Project-PartA/algorithms/search/BestFirstSearch.class
    	out/production/ATP-Project-PartA/algorithms/search/BreadthFirstSearch.class
    	out/production/ATP-Project-PartA/algorithms/search/DepthFirstSearch.class
    	out/production/ATP-Project-PartA/test/RunSearchOnMaze.class

--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -33,21 +33,27 @@ public class MyDecompressorInputStream extends InputStream {
 
         LinkedList<Integer> unCompressMaze= new LinkedList<>();
         for(int i=0; i<12 ; i++)
-            unCompressMaze.add(in.read());
+            unCompressMaze.add(array.get(i));
         //int sizeOfPair =in.read();
-        LinkedList<Pair<Integer,Integer>> dictionary = getDictionary(array,array.indexOf(12));
-        unCompressMaze.add(dictionary.get(0).getKey());
+        LinkedList<Pair<Integer,Integer>> dictionary = getDictionary(array,array.get(12));
         for(int i=1; i<dictionary.size(); i++){
             Stack<Integer> temp = new Stack<>();
             if(i != dictionary.size()-1)
                temp.add(dictionary.get(i).getKey());
             int p = dictionary.get(i).getValue();
             while (p>=1){
+
                 temp.add(dictionary.get(p).getKey());
                 p=dictionary.get(p).getValue();
             }
-            while (!temp.isEmpty())
-                unCompressMaze.add(temp.pop());
+            while (!temp.isEmpty()) {
+                if(temp.peek()==2){
+                    temp.pop();
+                }
+                else {
+                    unCompressMaze.add(temp.pop());
+                }
+            }
         }
         //byte []newByteMaze = new byte[byteArr.size()];
         for(int i=0; i<byteArray.length && i<unCompressMaze.size(); i++){
@@ -60,17 +66,14 @@ public class MyDecompressorInputStream extends InputStream {
 
     private LinkedList<Pair<Integer,Integer>>getDictionary(ArrayList<Integer> array ,int sizeOfUnit)throws IOException{
         LinkedList<Pair<Integer,Integer>> dictionary = new LinkedList<>();
-        int pos=16;
-//        in.read();
-//        in.read();
-//        in.read();
-        while(pos<array.size()){
-                int val=array.indexOf(pos++);
+        int pos=13;
+        while(pos<array.size()-1){
+                int val=array.get(pos++);
                 if(val==-1)
                     break;
-                int p=array.indexOf(pos++) & 0xFF;
-                for(int i=sizeOfUnit-1; i>=1; i--)
-                    p = (p & 0xFF<<(8)) | (array.indexOf(pos++) & 0xFF);
+                int p=array.get(pos++) & 0xFF;
+                for(int i=sizeOfUnit-2; i>=1; i--)
+                    p = (p << 8) | (array.get(pos++) & 0xFF);
                 dictionary.add(new Pair<>(val,p));
         }
         return dictionary;
