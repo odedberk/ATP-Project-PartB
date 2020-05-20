db7ae0a53aeb3a97d20510a54859d349e923e796
oded <odedodge@gmail.com>
2020-05-20 18:25:21 +0300

    updated ServerStrategySolveSearchProblem

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -7,5 +7,6 @@ public class Client {
     }
 
     public void communicateWithServer() {
+
     }
 }
--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -4,9 +4,12 @@ import algorithms.mazeGenerators.*;
 import algorithms.search.*;
 
 import java.io.*;
+import java.util.Hashtable;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerStrategySolveSearchProblem implements IServerStrategy {
-
+    public static Hashtable<byte[],String> solvedMazes= new Hashtable<>(); //<Maze,filePath>
+    private static AtomicInteger fileID = new AtomicInteger(0);
     @Override
     public void handleClient(InputStream inputStream, OutputStream outputStream) throws IOException {
         ObjectInputStream fromClient = new ObjectInputStream(inputStream);
@@ -17,8 +20,9 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        SearchableMaze searchableMaze = new SearchableMaze(toSolve);
         ISearchingAlgorithm solver = getSolver();
-        Solution sol = solver.solve(new SearchableMaze(toSolve));
+        Solution sol = getOrSolve(searchableMaze,solver);
         try {
                 toClient.writeObject(sol);
                 toClient.flush();
@@ -40,5 +44,44 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy {
             solver = new DepthFirstSearch();
         }
         return solver;
+    }
+
+    private Solution getOrSolve(SearchableMaze toSearch, ISearchingAlgorithm solver){
+        boolean exists=false;
+        Solution solution=null;
+        byte[] maze = toSearch.getMaze().toByteArray();
+        if (solvedMazes.contains(maze)){
+            exists=true;
+            try {
+                FileInputStream fileInputStream = new FileInputStream(System.getProperty("java.io.tmpdir")+"\\"+solvedMazes.get(maze));
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+                solution = (Solution)objectInputStream.readObject();
+                objectInputStream.close();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        if (!exists) {
+            solution=solver.solve(toSearch);
+            saveToFile(toSearch.getMaze(), solution);
+        }
+        return solution;
+    }
+
+    private void saveToFile (Maze toSave, Solution sol){
+        String fileName = "mazeSol_"+fileID.getAndIncrement();
+        try {
+            FileOutputStream outputStream = new FileOutputStream(System.getProperty("java.io.tmpdir")+"\\"+fileName);
+            ObjectOutputStream objectOutput = new ObjectOutputStream(outputStream);
+            objectOutput.writeObject(sol);
+            objectOutput.flush();
+            objectOutput.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        solvedMazes.put(toSave.toByteArray(),fileName);
     }
 }
