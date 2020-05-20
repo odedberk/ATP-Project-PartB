1db7ab4b403968a3884a19180d1b830c3c69163b
oded <odedodge@gmail.com>
2020-05-20 18:37:29 +0300

    updated Client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,12 +1,32 @@
 package Client;
 
+import java.io.IOException;
 import java.net.*;
 
 public class Client {
-    public Client(InetAddress localHost, int i, IClientStrategy iClientStrategy) {
+    private InetAddress address;
+    private int serverPort;
+    private IClientStrategy clientStrategy;
+
+    public Client(InetAddress address, int serverPort, IClientStrategy clientStrategy) {
+        this.address = address;
+        this.serverPort = serverPort;
+        this.clientStrategy = clientStrategy;
     }
 
     public void communicateWithServer() {
+        start();
+    }
 
+    public void start()
+    {
+        try {
+            Socket socket = new Socket(address,serverPort);
+            System.out.println("Client is connected to server!");
+            clientStrategy.clientStrategy(socket.getInputStream(),socket.getOutputStream());
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
