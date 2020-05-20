0a0e4d4950cb0068330b934aad75794d0eb910ef
oded <odedodge@gmail.com>
2020-05-20 15:51:44 +0300

    added Server and Client packages + Configurations class

--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -1,4 +1,11 @@
 package Server;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class ServerStrategyGenerateMaze implements IServerStrategy {
+    @Override
+    public void handleClient(InputStream inputStream, OutputStream outputStream) {
+
+    }
 }
--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -1,4 +1,32 @@
 package Server;
 
+import algorithms.mazeGenerators.Maze;
+
+import java.io.*;
+
 public class ServerStrategySolveSearchProblem implements IServerStrategy {
+
+    @Override
+    public void handleClient(InputStream inputStream, OutputStream outputStream) throws IOException {
+        ObjectInputStream fromClient = new ObjectInputStream(inputStream);
+        ObjectOutputStream toClient = new ObjectOutputStream(outputStream);
+        try {
+            Maze toSolve = (Maze)fromClient.readObject();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String phrase;
+        try {
+            String reversedPhrase;
+            while (!(phrase = fromClient.readLine()).equals("Thanks!")) {
+                reversedPhrase = new StringBuilder(phrase).reverse().toString();
+                toClient.writeObject(reversedPhrase+"\r\n");
+                toClient.flush();
+            }
+            toClient.writeObject("you welcome, bye!\r\n");
+            toClient.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
