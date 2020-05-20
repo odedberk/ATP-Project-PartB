2aab2c5df20b954588be1fd05d4bc4c96f6a0910
eilam gal <eilamg@post.bgu.ac.il>
2020-05-20 15:45:21 +0300

    new c

--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -1,4 +1,41 @@
 package Server;
 
+import IO.MyCompressorOutputStream;
+import algorithms.mazeGenerators.AMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+
 public class ServerStrategyGenerateMaze implements IServerStrategy {
+    private AMazeGenerator generator = new MyMazeGenerator();
+
+    public ServerStrategyGenerateMaze() {}
+
+    public AMazeGenerator getGenerator() {
+        return generator;
+    }
+
+    public void setGenerator(AMazeGenerator generator) {
+        this.generator = generator;
+    }
+
+    @Override
+    public void handleClient(InputStream inputStream, OutputStream outputStream) {
+        MyCompressorOutputStream out = new MyCompressorOutputStream(outputStream);
+        try {
+            ObjectInputStream input= new ObjectInputStream(inputStream);
+            int [] sizes = (int[]) input.readObject();
+            Maze newMaze = generator.generate(sizes[0],sizes[1]);
+            out.write(newMaze.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
