954227beedf03b1e315734f5ab6a3c21cd3b1a6e
oded <odedodge@gmail.com>
2020-05-20 16:23:18 +0300

    updated Configurations class

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -1,5 +1,28 @@
 package Server;
 
-public class Configurations {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
 
+public class Configurations {
+    private static Properties prop;
+    private static void initProp(){
+        try (InputStream input = Configurations.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null)
+                return;
+            prop = new Properties();
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void setProperty(String name, String val) {
+        initProp();
+        prop.setProperty(name,val);
+    }
+
+    public static String getProperty(String name){
+        return prop.getProperty(name);
+    }
 }
