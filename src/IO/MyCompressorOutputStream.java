740d96a7df83596ba24592899de4549ab3872d8b
oded <odedodge@gmail.com>
2020-05-25 18:34:34 +0300

    updated compression methods

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -31,57 +31,6 @@ public class MyCompressorOutputStream extends OutputStream {
         }
     }
 
-//    @Override
-//    public void write(byte[] b) {
-//        if (b.length<12 || b==null)
-//            try {
-//                throw new IOException();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//        Map<String,Integer> codes = new LinkedHashMap<>();
-//        ArrayList<Pair<Integer,Integer>> array = new ArrayList<>();
-//        int arrIndex=0;
-//        array.add(arrIndex++,new Pair<>(0,0));
-//        int k=12;
-//        codes.put(String.valueOf(b[k]),arrIndex);
-//        array.add(arrIndex++,new Pair((int)b[k++],0));
-//
-//        for (; k<b.length; k++){
-//            int pointer=0;
-//            String current = String.valueOf(b[k]);
-//            while (codes.containsKey(current) && k<b.length-1){ // 0 00 01 010
-//                pointer=codes.get(current);
-//                k++;
-//                current+=String.valueOf(b[k]);
-//            }
-//            if (codes.containsKey(current) && k==b.length-1) {
-//                array.add(arrIndex, new Pair(2, codes.get(current)));
-//                codes.put(current,arrIndex);
-//            }
-//            else{
-//                array.add(arrIndex,new Pair((int)b[k],pointer));
-//                codes.put(current,arrIndex++);
-//            }
-//        }
-//
-//        //Write compressed array//
-//        for (int i=0; i< 12 ; i++) //maze dimensions + start/goal
-//            write(b[i]);
-//
-//        int pointerSize= array.size()>256 ? (array.size()>65536 ? 3 : 2) : 1; // how many bytes to represent pointer
-//        write(pointerSize+1); //pointer + val {0/1}
-////        System.out.println("DEBUG : array size:" + array.size()+" unit size: "+(pointerSize+1)); //DEBUGGING
-//        for (Pair<Integer, Integer> Pair : array) { //send array
-//            write(Pair.getKey());
-//            for (int j = pointerSize - 1; j >= 0; j--)
-//                write((byte) (Pair.getValue() >> 8 * j));
-//        }
-////        out.close();
-//        //Finish writing compressed array//
-//    }
-
     @Override
     public void write(byte[] b) {
         if (b.length<12 || b==null)
@@ -90,19 +39,80 @@ public class MyCompressorOutputStream extends OutputStream {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        //Write compressed array//
-        int i=0;
-        for (; i< 12 ; i++) //maze dimensions + start/goal
-            write(b[i]);
-        while (i<b.length){
-            byte cell=b[i++];
-            for (int j =0 ; j<7; j++){
-                if (i<b.length)
-                    cell=(byte)((cell<<1)|b[i++]);
-                else
-                    cell=(byte)((cell<<1)|0);
+
+        Map<String,Integer> codes = new LinkedHashMap<>();
+        ArrayList<Pair<Integer,Integer>> array = new ArrayList<>();
+        int arrIndex=0;
+        array.add(arrIndex++,new Pair<>(0,0));
+        int k=12;
+        codes.put(String.valueOf(b[k]),arrIndex);
+        array.add(arrIndex++,new Pair((int)b[k++],0));
+
+        for (; k<b.length; k++){
+            int pointer=0;
+            String current = String.valueOf(b[k]);
+            while (codes.containsKey(current) && k<b.length-1){ // 0 00 01 010
+                pointer=codes.get(current);
+                k++;
+                current+=String.valueOf(b[k]);
             }
-            write(cell);
+            if (codes.containsKey(current) && k==b.length-1) {
+                array.add(arrIndex, new Pair(2, codes.get(current)));
+                codes.put(current,arrIndex);
+            }
+            else{
+                array.add(arrIndex,new Pair((int)b[k],pointer));
+                codes.put(current,arrIndex++);
+            }
         }
+
+        //Write compressed array//
+        for (int i=0; i< 12 ; i++) //maze dimensions + start/goal
+            write(b[i]);
+
+        int pointerSize= array.size()>128 ? (array.size()>32768 ? 3 : 2) : 1; // how many bytes to represent pointer
+        write(pointerSize); //pointer + val {0/1}
+//        System.out.println("DEBUG : array size:" + array.size()+" unit size: "+(pointerSize+1)); //DEBUGGING
+        byte last=0;
+        for (Pair<Integer, Integer> Pair : array) { //send array
+//            write(Pair.getKey());
+            last=(byte)(Pair.getKey()<<0);
+            byte cell = (byte) (Pair.getKey()<<7);
+            cell = (byte) (cell | (Pair.getValue()>>(8*(pointerSize-1))));
+            write(cell);
+            for (int j = pointerSize - 2; j >= 0; j--) {
+                cell=(byte) (Pair.getValue() >> 8 * j);
+                write(cell);
+            }
+        }
+        write(last);
+
+//        out.close();
+        //Finish writing compressed array//
     }
+
+
+//    @Override
+//    public void write(byte[] b) {
+//        if (b.length<12 || b==null)
+//            try {
+//                throw new IOException();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        //Write compressed array//
+//        int i=0;
+//        for (; i< 12 ; i++) //maze dimensions + start/goal
+//            write(b[i]);
+//        while (i<b.length){
+//            byte cell=b[i++]; 10110100
+//            for (int j =0 ; j<7; j++){
+//                if (i<b.length)
+//                    cell=(byte)((cell<<1)|b[i++]);
+//                else
+//                    cell=(byte)((cell<<1)|0);
+//            }
+//            write(cell);
+//        }
+//    }
 }
--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -32,98 +32,99 @@ public class MyDecompressorInputStream extends InputStream {
         }
     }
 
-//    public int read(byte[] byteArray){
-//
-//        ArrayList<Integer> array = new ArrayList<>();
-//        int next = 0;
-//        try {
-//            next = in.read();
-//
-//        while (next!=-1) { // read received array
-//            array.add(next);
-//            next=in.read();
-//        }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        for(int i=0; i<12 ; i++) { // get dimensions
-//            int temp = array.get(i);
-//            byteArray[i] =(byte)temp;
-//        }
-//        LinkedList<Pair<Integer,Integer>> dictionary = getDictionary(array,array.get(12));
-//        ArrayList<String> deCompress = new ArrayList<>();
-//        deCompress.add("");
-//        for(int i=1; i<dictionary.size(); i++) {
-//            String temp = "";
-//            if (i != dictionary.size() - 1)
-//                temp = "" + dictionary.get(i).getKey();
-//            else {
-//                if (dictionary.get(i).getKey() != 2)
-//                    temp = "" + dictionary.get(i).getKey();
-//            }
-//            if (dictionary.get(i).getValue() >= 1)
-//                temp = deCompress.get(dictionary.get(i).getValue()) + temp;
-//            deCompress.add(i, temp);
-//        }
-//        int insert=12;
-//        for (String temp : deCompress){
-//            for(char c : temp.toCharArray()) {
-//                if(c=='1')
-//                    byteArray[insert++]=1;
-//                else
-//                    byteArray[insert++]=0;
-//            }
-//        }
-//
-//        return 0;
-//    }
+    public int read(byte[] byteArray){
+
+        ArrayList<Integer> array = new ArrayList<>();
+        int next = 0;
+        try {
+            next = in.read();
+
+        while (next!=-1) { // read received array
+            array.add(next);
+            next=in.read();
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for(int i=0; i<12 ; i++) { // get dimensions
+            int temp = array.get(i);
+            byteArray[i] =(byte)temp;
+        }
+        LinkedList<Pair<Integer,Integer>> dictionary = getDictionary(array,array.get(12));
+        ArrayList<String> deCompress = new ArrayList<>();
+        deCompress.add("");
+        for(int i=1; i<dictionary.size(); i++) {
+            String temp = "";
+            if (i != dictionary.size() - 1)
+                temp = "" + dictionary.get(i).getKey();
+            else {
+                if (dictionary.get(i).getKey() != 2)
+                    temp = "" + dictionary.get(i).getKey();
+            }
+            if (dictionary.get(i).getValue() >= 1)
+                temp = deCompress.get(dictionary.get(i).getValue()) + temp;
+            deCompress.add(i, temp);
+        }
+        int insert=12;
+        for (String temp : deCompress){
+            for(char c : temp.toCharArray()) {
+                if(c=='1')
+                    byteArray[insert++]=1;
+                else
+                    byteArray[insert++]=0;
+            }
+        }
+
+        return 0;
+    }
 
     private LinkedList<Pair<Integer,Integer>>getDictionary(ArrayList<Integer> array ,int sizeOfUnit){
         LinkedList<Pair<Integer,Integer>> dictionary = new LinkedList<>();
         int pos=13+sizeOfUnit;
 
         dictionary.add(new Pair<>(0,0));
-        while(pos<array.size()){
-                int val=array.get(pos++);
+        while(pos<array.size()-1){
+                int val=(array.get(pos)>>7)&1;
                 if(val==-1)
                     break;
-                int p=array.get(pos++) & 0xFF;
-                for(int i=sizeOfUnit-2; i>=1; i--)
+                int p=(array.get(pos++)&127) & 0xFF;
+                for(int i=sizeOfUnit-2; i>=0; i--)
                     p = (p <<(8)) | (array.get(pos++) & 0xFF);
                 dictionary.add(new Pair<>(val,p));
         }
         return dictionary;
     }
-
-    public int read(byte[] byteArray) {
-
-        ArrayList<Byte> array = new ArrayList<>();
-        int next = 0;
-        try {
-            next = in.read();
-            while (next != -1) { // read received array
-                array.add((byte) next);
-                next = in.read();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int i = 0;
-        for (; i < 12; i++) { // get dimensions
-            int temp = array.get(i);
-            byteArray[i] = (byte) temp;
-        }
-        int rowSize = ((array.get(0)& 0xFF) <<8) | (array.get(1) & 0xFF);
-        int colSize = ((array.get(2) & 0xFF) <<8) | (array.get(3) & 0xFF);
-        int size = rowSize*colSize;
-        int k=0;
-        while(k<size && i<array.size()){
-            byte cell=array.get(i++);
-            for (int j =7 ; j>=0 && k<size; j--,k++){
-                byteArray[12+k]= (byte) (1&cell>>j);
-            }
-        }
-
-    return 0;
-    }
+//
+//    public int read(byte[] byteArray) {
+//
+//        ArrayList<Byte> array = new ArrayList<>();
+//        int next = 0;
+//        try {
+//            next = in.read();
+//            while (next != -1) { // read received array
+//                array.add((byte) next);
+//                next = in.read();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        int i = 0;
+//        for (; i < 12; i++) { // get dimensions
+//            int temp = array.get(i);
+//            byteArray[i] = (byte) temp;
+//        }
+//        int rowSize = ((array.get(0)& 0xFF) <<8) | (array.get(1) & 0xFF);
+//        int colSize = ((array.get(2) & 0xFF) <<8) | (array.get(3) & 0xFF);
+//        int size = rowSize*colSize;
+//        int k=0;
+//        while(k<size && i<array.size()){
+//            byte cell=array.get(i++);
+//            for (int j =7 ; j>=0 && k<size; j--,k++){
+//                byteArray[12+k]= (byte) (1&cell>>j);
+//            }
+//        }
+//        return 0;
+//    }
 }
+
+
